32380d9ff7d121eac7f3b69d46b23ec47d88a980
iSAD <bradx2713@gmail.com>
2024-06-26 17:07:57 +0200

    Initial Commit

---
+++ KnowledgeKwik/KnowledgeKwikApp.swift
@@ -0,0 +1,17 @@
+//
+//  KnowledgeKwikApp.swift
+//  KnowledgeKwik
+//
+//  Created by Tafadzwa Alexander on 2024/06/26.
+//
+
+import SwiftUI
+
+@main
+struct KnowledgeKwikApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
